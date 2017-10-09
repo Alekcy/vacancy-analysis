0b2fd2dc8bfb8d162ad1dac24704a4f064126969
Alekcy <alekcyvolkov@gmail.com>
2017-10-09 21:23:14 +0300

    update license and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,4 @@
-/*const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'src')
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
-  },
-   resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
-  watch:true,
-  watchOptions: {
-  aggregateTimeout: 300,
-  poll: 1000
-  }
-};*/
 var path = require('path')
 var webpack = require('webpack')
 
